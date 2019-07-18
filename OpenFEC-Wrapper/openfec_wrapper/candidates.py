eefd75b57ecb9be796a487ed848279e20f880574
Walker Lambrecht <walkerlambrecht@gmail.com>
2019-07-18 11:39:29 -0400

    Added the core files for the OpenFEC_Wrapper
    This wrapper currently only can download the entire list of candidates, but if in the future DataUSA may want to provide information on financial data for candidates, the API wrapper could easily be updated to handle this.

---
+++ OpenFEC-Wrapper/openfec_wrapper/candidates.py
@@ -0,0 +1,41 @@
+# openfec_wrapper/fec.py
+import pandas as pd
+from pandas.io.json import json_normalize
+from .utils import session
+
+
+class CANDIDATES(object):
+    def __init__(self, office):
+        self.office = office
+
+    # Returns information on all candidates that ran for a specific office
+    def dataframe(self):
+        path = 'https://api.open.fec.gov/v1/candidates/?office={}'.format(
+            self.office)
+        response = session.get(
+            path, headers=session.headers, params=session.params)
+        data = response.json()['results']
+        results = json_normalize(data)
+        print("COMPLETED INITIAL DATA GRAB")
+        return candidate_recur(self, 2, results, session.params, session.headers)
+
+
+# Recursive helper function for candidates method
+def candidate_recur(self, page, df, params, headers):
+    print("RECURRENCE NUMB: " + str(page))
+    path = 'https://api.open.fec.gov/v1/candidates/?office={}'.format(
+        self.office)
+    params['page'] = str(page)
+    response = session.get(path, headers=headers, params=params)
+    data = response.json()['results']
+    results = json_normalize(data)
+
+    if page % 10 == 0 & page > 9:
+        print("Retrieved pages: " + str(page) + " through " + str(page - 10))
+    if not results.empty:
+        print("NEXT PAGE")
+        frames = [df, results]
+        df = pd.concat(frames)
+        return candidate_recur(self, page + 1, df, params, headers)
+    else:
+        return df
---
+++ OpenFEC-Wrapper/openfec_wrapper/utils.py
@@ -0,0 +1,32 @@
+# openfec_wrapper/utils.py
+import os
+import requests
+
+FEC_API_KEY = os.environ.get('FEC_API_KEY', None)
+
+
+class APIKeyMissingError(Exception):
+    pass
+
+
+if FEC_API_KEY is None:
+    raise APIKeyMissingError(
+        "All api requests require an API key. See "
+        "https://api.data.gov/signup/"
+        "to retrieve an authentication token from "
+        "the Federal Election Commision"
+    )
+
+session = requests.Session()
+session.params = {
+    'per_page': '100',
+    'sort_nulls_last': 'false',
+    'sort': 'name',
+    'page': '1',
+    'api_key': FEC_API_KEY,
+    'sort_null_only': 'false',
+    'sort_hide_null': 'false',
+}
+session.headers = {
+    'accept': 'application/json',
+}
