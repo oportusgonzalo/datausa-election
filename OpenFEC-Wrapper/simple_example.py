86eca0a652cb62e5512427b42197d4dc70809d68
Walker Lambrecht <walker@datawheel.us>
2019-07-22 10:41:02 -0400

    Updated examples
    Updated the examples in this file to work with the new methods used when creating CandidateData class.

--- OpenFEC-Wrapper/simple_example.py
+++ OpenFEC-Wrapper/simple_example.py
@@ -1,18 +1,21 @@
 # A simple example of how to use the openFEC wrapper
-from openfec_wrapper import CANDIDATES
+from openfec_wrapper import CandidateData
 
 # Create CANDIDATE objects for senate, house, and president
-presidential_candidates = CANDIDATES('P')
-senate_candidates = CANDIDATES('S')
-house_candidates = CANDIDATES('H')
-
-
-# Retrieve and parse JSON data from FEC. Convert to pandas dataframes.
-president = presidential_candidates.dataframe()
-senate = senate_candidates.dataframe()
+p_candidates = CandidateData.presidential_candidates()
+s_candidates = CandidateData.senate_candidates()
 
 # Commented out house data as it is a large download that may result in a
-# "429 Too many requests" API Error.
-# house = house_candidates.dataframe()
+# "429 Too many requests" API Error. This is handled by using an
+# "upgraded key" from the FEC. Email apiinfo@fec.gov to find out about
+# getting an upgraded key.
+# h_candidates = CandidateData.house_candidates()
 
-print(president.head())
+# Retrieve and parse JSON data from FEC. Convert to pandas dataframes.
+president = p_candidates.dataframe()
+senate = s_candidates.dataframe()
+# house = h_candidates.dataframe()
+
+print(president.head(20))
+print(senate.head(20))
+# print(house.head(20))
